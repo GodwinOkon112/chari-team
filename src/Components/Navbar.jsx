85d21a3d217aa2be9099667e5685af242f5252fe
win_boy <godwinokon112@gmail.com>
2024-10-05 15:31:17 +0100

    another commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,13 +19,13 @@ const NavBar = () => {
     <div>
         
       <TopNav />
-      <div className='flex justify-between sm:py-4 py-0 gap-[10rem] items-start'>
+      <div className='flex justify-between sm:py-4 py-0 gap-[10rem] pt-8 items-start'>
         <h2 className='text-[#e27126] sm:text-3xl text-4xl pt-[1px] font-bold w-[30%]'>
           Chari<span className='text-white'>Team</span>
         </h2>
         <h3 className='text-5xl sm:hidden cursor-pointer ' onClick={toggler} >&#8801;</h3>
 
-        <ul className='sm:flex hidden sm:flex-row ul flex-col sm:items-center sm:justify-end items-end sm:pt-[1px] pt-[5rem] sm:pr-[auto] pr-[2rem]  sm:h-[auto] w-[80%] h-[100vh]  m-auto sm:relative fixed left-[-5rem]  sm:left-0 bg-[#04333acc]  sm:bg-transparent sm:gap-5 gap-4 font-semibold sm:text-lg  text-[2rem] flex-wrap'>
+        <ul className='sm:flex hidden sm:flex-row ul flex-col sm:items-center sm:justify-end items-end sm:pt-[1px] pt-[5rem] sm:pr-[auto] pr-[2rem]  sm:h-[auto] w-[90%] h-[100vh]  m-auto sm:relative fixed left-[-5rem]  sm:left-0 bg-[#020607fd]  sm:bg-transparent sm:gap-5 gap-4 font-semibold sm:text-lg top-0  text-[1.5rem] flex-wrap'>
           <li className='text-[#e27126]'>Home</li>
           <li>About</li>
           <li>Causes</li>
